4198b0a86036953c9e9cf25692435f3b397905e5
Nathaniel Branscum <nathaniel.d.branscum.24@dartmouth.edu>
2022-10-26 16:01:43 -0400

    Fixed typo that broke everything

--- Assets/Scripts/SolSystem.cs
+++ Assets/Scripts/SolSystem.cs
@@ -21,33 +21,23 @@ public class SolSystem : MonoBehaviour
         
     }
 
-    private (double, double) calcDistance(Body body1, Body body2)
+    private (double distX, double distZ) calcDistance(Body body1, Body body2)
     {
-        return (distX : body2.x - body1.x, distZ : body2.z - body1.z);
+        return (body2.x - body1.x, body2.z - body1.z);
     }
 
-    private (double, double) calcAcceleration(Body body, double dx, double dz)
+    private (double ax, double az) calcAcceleration(Body body, double dx, double dz) 
     {
-        private double totalDistance = Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dz, 2));
-        private double ax, az;
+        double totalDistance = Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dz, 2));
+        double ax, az;
 
-        if (dx != 0) 
-        {
-            ax = ((G * body.mass * dx) / (Math.Pow(total_distance, 3)));
-        } 
-        else 
-        { 
-            ax = 0; 
-        }
-        if (dz != 0) 
-        {
-            az = ((G * body.mass * dz) / (Math.Pow(total_distance, 3)));
-        } 
-        else 
-        { 
-            az = 0;
-        }
+        if (dx != 0) {
+            ax = ((G * body.mass * dx) / (Math.Pow(totalDistance, 3)));
+        } else { ax = 0; }
+        if (dz != 0) {
+            az = ((G * body.mass * dz) / (Math.Pow(totalDistance, 3)));
+        } else { az = 0;}
 
-        return (ax : ax, az : az);
+        return (ax, az);
     }
 }
